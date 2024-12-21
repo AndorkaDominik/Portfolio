85fbbbd7c1dfeef6b1088771a33f1d29d75f455f
Mador <dominik.andorka@gmail.com>
2024-12-21 18:24:58 +0100

    Preload css 6

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 // src/components/Home.js
 import React, { useEffect, useState } from 'react';
 import './Home.css'
-import '/backgroundChange.css'
+import '/public/backgroundChange.css'
 
 import { motion } from 'framer-motion';
 import { Typewriter } from 'react-simple-typewriter'
