0c49fcec108d2d9fb2398811af856028b8f4069a
Mador <dominik.andorka@gmail.com>
2024-12-21 15:08:33 +0100

    Resolve issue

--- src/components/ProjectHeader/ProjectHeader.jsx
+++ src/components/ProjectHeader/ProjectHeader.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import "./ProjectHeader.css"
-import Click from '../../assets/Click.png'
+import Click from '../../assets/click.png'
 
 const Project = ({title, descript, team, img, website}) => {
 
